de0d9ea713d38ffbff71213c1aa24101ae1a8202
Joenel <joenelsevellejo831@gmail.com>
2025-02-18 11:10:33 +0800

    added SpeedInsights

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import QueryProvider from "./QueryProvider/Provider";
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -50,6 +51,7 @@ export default function RootLayout({
       >
         <QueryProvider>
         {children}
+        <SpeedInsights />
         </QueryProvider>
       </body>
     </html>
