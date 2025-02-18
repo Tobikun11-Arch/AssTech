dbb11691be3132c0eeb68d07ae365ef6bf66e5ca
Joenel <joenelsevellejo831@gmail.com>
2025-02-18 14:32:22 +0800

    Added cache for github

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,11 @@ import React from 'react'
 import Home from './components/Homepage/Home'
 
 export default async function Page() {
-    const response = await fetch('https://api.github.com/repos/Tobikun11-Arch/AssTech/stargazers')
+    const response = await fetch('https://api.github.com/repos/Tobikun11-Arch/AssTech/stargazers', {
+        cache: "no-store"
+    })
     const data = await response.json()
+
 
     return <Home count={data.length}/>
 }
